e89363489c6cba48081fa5b1384aaaf291ff77a7
Raajhesh Kannaa Chidambaram <raajheshkannaa.aws@gmail.com>
2021-09-29 11:37:51 +0530

    code push

---
+++ cdk_bootstrap_multiple_accounts.py
@@ -0,0 +1,85 @@
+import boto3
+import subprocess
+
+trusted_account = '<ENTER THE ACCOUNT ID YOU WILL RUN CI/CD PIPELINES FROM>'
+
+def get_org_accounts(session):
+	org_client = session.client('organizations')
+
+	results = []
+	messages = []
+	paginator = org_client.get_paginator('list_accounts')
+	response_iterator = paginator.paginate()
+
+	for response in response_iterator:
+		results = results + response['Accounts']
+
+	for index in results:
+		messages = messages + (index['Id']).split()
+
+	return messages
+
+def assume_role(session, aws_account_number, role_name):
+	resp = session.client('sts').assume_role(
+		RoleArn='arn:aws:iam::{}:role/{}'.format(aws_account_number,role_name),
+		RoleSessionName='CDKBootstrappin')
+	
+	# Storing STS credentials
+	creds = boto3.Session(
+		aws_access_key_id = resp['Credentials']['AccessKeyId'],
+		aws_secret_access_key = resp['Credentials']['SecretAccessKey'],
+		aws_session_token = resp['Credentials']['SessionToken']
+	)
+
+	print("Assumed session for {}.".format(
+		aws_account_number
+	))
+
+	return creds, resp
+
+if __name__ == '__main__':
+
+	# Session credentials of current user and use this to assume roles.
+	org_session = boto3.Session()
+	accounts = get_org_accounts(org_session)
+
+	# Get the current AWS Organization Account ID
+	aws_org_account = boto3.client('sts').get_caller_identity()['Account']
+
+	# Used to obtain the list of AWS Regions using the EC2 service.
+	ec2 = org_session.client('ec2', region_name='us-east-1')
+	regions = ec2.describe_regions()['Regions']
+	
+	for account in accounts:
+		try:
+			account = account.strip()
+
+			# Session of the assumed IAM Role in the corresponding member account using the session(OrganizationAccountAccessRole/ControlTowerExecution) role.
+			session, resp = assume_role(org_session, account, 'AWSControlTowerExecution') # This is under the assumption we have a Control Tower and accounts are enrolled. 
+			# Under normal conditions this should be 'OrganizationAccountAccessRole'
+			
+			# Credentials of the assumed role which will be used to set environment variables. 
+			aws_access_key_id = str(resp['Credentials']['AccessKeyId'])
+			aws_secret_access_key = str(resp['Credentials']['SecretAccessKey'])
+			aws_session_token = str(resp['Credentials']['SessionToken'])
+
+			# Iterate CDK Bootstrapping for all regions.
+			# Comment out this `for` loop and Shift-Tab below section, if bootstrapping is not necessary for all regions.
+			for region in regions:
+				region_name = region['RegionName']
+
+				'''
+				Export environment variables
+				* AWS_ACCESS_KEY_ID
+				* AWS_SECRET_ACCESS_KEY
+				* AWS_SESSION_TOKEN
+
+				Execute `cdk bootstrap aws://<account>/<region> --cloudformation-execution-policies arn:aws:iam::aws:policy/AdministratorAccess --trust <CI/CD AWS Account>`
+				'''
+				command = "export AWS_ACCESS_KEY_ID=" + aws_access_key_id + ";export AWS_SECRET_ACCESS_KEY=" + aws_secret_access_key + ";export AWS_SESSION_TOKEN=" + aws_session_token + "; cdk bootstrap aws://" + account + "/" + region_name + " --cloudformation-execution-policies arn:aws:iam::aws:policy/AdministratorAccess --trust " + trusted_account
+				
+				# Execute the command in a single process shell.
+				aws_cli = subprocess.run(command, shell=True)
+
+		except:
+			pass
